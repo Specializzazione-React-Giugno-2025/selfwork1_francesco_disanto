efbefce9fd7019d366fc5ace6c17477efb6e0a35
francescodisanto96 <francesco.disanto96@agmail.com>
2025-06-28 17:17:25 +0200

    => al posto di return

--- script_2.js
+++ script_2.js
@@ -4,6 +4,4 @@ export const function_1 = (obj, lista_nomi) => {
   return { ...obj, contatti: lista_nomi };
 };
 
-export const function_2 = ({ contatti }) => {
-  return `I contatti sono: ${contatti}`;
-};
+export const function_2 = ({ contatti }) => `I contatti sono: ${contatti}`;
